8bd49f297b5b10280e216d2d503a07e186950262
Phap Duong <d2phap@gmail.com>
2019-04-20 19:33:49 +0800

    updated donation section

--- Source/laravel/resources/views/shared/_donation.blade.php
+++ Source/laravel/resources/views/shared/_donation.blade.php
@@ -13,11 +13,30 @@
             <span class="text">Donate <b>$10</b> via <span class="icon-paypal"></span><i class="fw-bold">aypal</i></span>
         </a>
     </div>
-    
+
     <div class="mt-3">
-        <a href="https://teespring.com/stores/imageglass" class="btn btn-primary" target="_blank" rel="noopener noreferrer">
-            <span class="text">Buy a gift <span class="icon-gift"></span></span>
-        </a>
+        <table class="table table-bg text-left d-inline-block">
+            <tbody>
+                <tr>
+                    <td class="text-nowrap">
+                        <img src="https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png" alt="Bitcoin" width="20" height="20"> 
+                        Bitcoin
+                    </td>
+                    <td>
+                        <code class="text-break-all">3Hp7QLP9Fxpb1s4CAcZFYR7NUgN1bR93Hy</code>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-nowrap">
+                        <img src="https://dynamic-assets.coinbase.com/7796fb1bd5752a156c77e0b1fa28fb17e93d03b4b8e2dcff58dcaf8f99b2e4a3c3af2cd905d5536761c54ac273d62d91a38ef8e5702fa988c37264e7aba36b3b/asset_icons/3af4b33bde3012fd29dd1366b0ad737660f24acc91750ee30a034a0679256d0b.png" alt="Ethereum" width="20" height="20"> 
+                        Ethereum
+                    </td>
+                    <td>
+                        <code class="text-break-all">0x6DDBd646586bebF7a97e022D615cbE789D39a965</code>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 
 </div>
