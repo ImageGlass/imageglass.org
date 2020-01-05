36128d07025790e4bc1438f9d6017d2e71fcbbca
Phap Duong <d2phap@gmail.com>
2020-01-05 19:32:07 +0800

    updated docs: features for v7.5

--- Source/laravel/resources/views/pages/docs/features.blade.php
+++ Source/laravel/resources/views/pages/docs/features.blade.php
@@ -2,7 +2,6 @@
 @section("_body_content")
 
 <section class="container py-4 py-lg-5">
-
     <div class="row no-gutters">
         <h2 class="fs-h1 mb-4">
             Features
@@ -16,34 +15,54 @@
             <p>ImageGlass is another open source basic image viewer, which, while simple, benefits from the speed that comes with being so lightweight, and is a good choice for Windows users.</p>
 
             <div class="mt-3 embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/d2ZCAZE0el4?rel=0&amp;showinfo=0" title="Introduction video"></iframe> 
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/cNXpHBMcjAI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-
 
             <h3 id="image-viewing" class="mt-5 mb-3 fs-h3">Image viewing</h3>
             <ul>
-                <li>Supports over <a href="{{ url('/documentation/supported-formats') }}" title="View all supported image formats" target="_blank">70+ common image formats</a>, including PNG, GIF, SVG*, WEBP*, HEIC, RAW,...</li>
+                <li>Supports over <a href="{{ url('/docs/supported-formats') }}" title="View all supported image formats">70+ common image formats</a>, including PNG, GIF, SVG*, WEBP*, HEIC, RAW,...</li>
                 <li>Losslessly rotate / save the viewing image.</li>
                 <li>Ability of opening image from clipboard directly.</li>
                 <li>Rapidly switch between images.</li>
-                <li>Drag-n-Drop viewing image supported.</li>
+                <li>Drag-n-drop viewing image supported.</li>
                 <li>Real-time update the viewing image.</li>
                 <li>Works great with GIF format: extract image frames, play/pause animation.</li>
-
-                <li class="mt-1">5 zoom modes: Auto Zoom, Scale to Width, Scale to Height, Zoom to Fit, Lock Zoom Ratio.</li>
-                <li>Various image navigation options: shortcuts, mousewheel</li>
+                <li>Ability to view / extract multi-page image files (TIF, ICO) separately.</li>
+                <li>
+                    6 zoom modes:
+                    <ul>
+                        <li>Auto Zoom</li>
+                        <li>Scale to Width</li>
+                        <li>Scale to Height</li>
+                        <li>Scale to Fit</li>
+                        <li>Scale to Fill</li>
+                        <li>Lock Zoom ratio</li>
+                    </ul>
+                </li>
+                <li class="mt-1">
+                    4 combinable window modes:
+                    <ul>
+                        <li>Window fit: Auto-fit the window to the viewing image size</li>
+                        <li>Frameless: A borderless window</li>
+                        <li>Full screen</li>
+                        <li>Slideshow</li>
+                    </ul>
+                    Combining the above 4 window modes can create a new amazing window mode, such as: Frameless windowed slideshow, Window-fit slideshow,...
+                </li>
+                <li>Various image navigation options: shortcuts, mouse wheel</li>
                 <li>Horizontal / Vertical image scrolling.</li>
                 <li>Fast thumbnail preview.</li>
                 <li>Thumbnail images can be shown in the current viewing folder.</li>
                 <li>Color Picker tool allows user to pick and convert color to 4 formats: RGBA, HEXA, CMYK, HSLA.</li>
-                <li>Slideshow viewing functionality.</li>
-                <li>Conversion can be made in up to <a href="{{ url('/documentation/supported-formats') }}" title="View all supported image formats" target="_blank">10 different formats</a>.</li>
-                <li>Possibility of sharing the image being viewed to Facebook.</li>
-                <li><a href="{{ url('/documentation/ui-shortcuts-reference') }}" title="View all shortcuts" target="_blank">Quick shortcuts</a> supported.</li>
+                <li>Slideshow viewing with countdown timer.</li>
+                <li>Conversion can be made in up to <a href="{{ url('/docs/supported-formats') }}" title="View all supported image formats">10 different formats</a>.</li>
+                <li>Supported basic touch gestures.</li>
+                <li><a href="{{ url('/docs/ui-shortcuts-reference') }}" title="View all shortcuts">Quick shortcuts</a> supported.</li>
 
-                <li class="mt-1">Clean nice modern minimal UI; posibility of hide/show toolbar.</li>
-                <li><a href="{{ url('download/themes') }}" title="View all theme packs" target="_blank">Different theme packs</a> available to change the look of the interface.</li>
-                <li><a href="{{ url('download/languagepacks') }}" title="View all language packs" target="_blank">Multilanguage support</a>, which makes the user able to create its own packs.</li>
+                <li>Clean nice modern minimal UI with customizable toolbar.</li>
+                <li><a href="{{ url('download/themes') }}" title="View all theme packs">Different theme packs</a> available to change the look of the interface.</li>
+                <li><a href="{{ url('download/languagepacks') }}" title="View all language packs">Multilanguage support</a>, which makes the user able to create its own packs.</li>
+                <li>Ability to control ImageGlass over <a href="{{ url('docs/app-configs') }}" title="App configuration">configuration</a>, pre-define or even lock some settings at installation.</li>
             </ul>
 
 
--- Source/laravel/resources/views/shared/_blog-post-new.blade.php
+++ Source/laravel/resources/views/shared/_blog-post-new.blade.php
@@ -1,18 +1,18 @@
-@if (count(@$_blog_posts) > 0)
+{{-- @if (count(@$_blog_posts) > 0)
     @php
         $blog_post = @$_blog_posts[0];
         $post_total_days = getTotalDays($blog_post->pubDate);
     @endphp
 
-    @if ($post_total_days < 10)
+    @if ($post_total_days < 15)
     <section class="blog-post-new">
         <a href="{{ $blog_post->link }}" target="_blank" rel="noopener noreferrer">
-            <span class="tag-new">New blog!</span>
+            <span class="tag-new">💥 New!</span>
             <span class="title">{{ $blog_post->title }}</span>
         </a>
     </section>
     @endif
-@endif
+@endif --}}
 
 <section class="blog-post-new">
     <a href="https://medium.com/@d2phap/viewing-multi-page-tif-format-e0dcc256f4f0" target="_blank" title="Experiment viewing multi-page TIF format!" rel="noopener noreferrer">
