c92d66b49d261a33bbcbadeb3f712f8124c2c32a
Phap Dieu Duong <d2phap@gmail.com>
2021-04-18 17:42:34 +0800

    updated donation, add donorbox

--- Source/laravel/resources/views/modules/home/home-source.blade.php
+++ Source/laravel/resources/views/modules/home/home-source.blade.php
@@ -5,8 +5,8 @@
                 Free - Open Source Image Viewer
             </h2>
             <div class="text-center mt-4 fs-h4 fw-light">
-                ImageGlass is totally free for any purpose use.<br/>
-                Its source code was released under the terms of the <a class="link fw-normal" href="https://github.com/d2phap/ImageGlass/blob/master/LICENSE" target="_blank" rel="noopener">GNU General Public License v3.0</a>
+                ImageGlass and its source code are released<br/>
+                under the terms of the <a class="link fw-normal" href="https://github.com/d2phap/ImageGlass/blob/master/LICENSE" target="_blank" rel="noopener">GNU General Public License v3.0</a>
 
                 <div class="mt-4">
                     <a href="https://github.com/d2phap/ImageGlass" class="btn btn-primary" target="_blank" rel="noopener">
--- Source/laravel/resources/views/shared/_donation.blade.php
+++ Source/laravel/resources/views/shared/_donation.blade.php
@@ -3,40 +3,41 @@
 </h2>
 <div class="mt-4 fs-h4 fw-light">
     <p>
-        Developing and maintaining ImageGlass have been taking thousands of hours of my time and a large part of my sanity as well as my finance. If you love this little app, it would go a great way to ensuring that I can afford to take the time to continue to develop it.
+        ImageGlass is open source, free, ad-free photo viewer but developing and maintaining it do not. Your financial support gives me more motivations to work on the future releases.
     </p>
     <p class="">
-        Thanks for your gratitude and finance help!
+        You can become my sponsor in GitHub (one-time/monthly, no service fee), Patreon (monthly), PayPal,...
+        Thanks for your support!
     </p>
     <div>
-        <a href="https://patreon.com/d2phap" class="btn btn-primary mr-3" target="_blank" rel="noopener noreferrer">
-            Patreon
-            <i class="icon-patreon"></i>
-        </a>
-
-        <a href="https://github.com/sponsors/d2phap" class="btn btn-primary" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/sponsors/d2phap" class="btn btn-primary mr-2" target="_blank" rel="noopener noreferrer">
             Github
             <i class="icon-github"></i>
         </a>
+        <a href="https://patreon.com/d2phap" class="btn btn-primary mr-2" target="_blank" rel="noopener noreferrer">
+            Patreon
+            <i class="icon-patreon"></i>
+        </a>
+        <a href="https://www.paypal.me/d2phap" class="btn btn-primary" target="_blank" rel="noopener noreferrer">
+            <span class="text"><span class="icon-paypal"></span><i class="fw-bold">aypal</i></span>
+        </a>
     </div>
     <div class="mt-3">
-        <a href="https://www.paypal.me/ddphap" class="btn btn-primary" target="_blank" rel="noopener noreferrer">
-            <span class="text">Donate <b>10 USD</b> via <span class="icon-paypal"></span><i class="fw-bold">aypal</i></span>
+        <script type="text/javascript" defer src="https://donorbox.org/install-popup-button.js"></script>
+        <a class="dbox-donation-button btn btn-primary d-inline-flex align-items-center" href="https://donorbox.org/imageglass?default_interval=o&hide_donation_meter=true">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                <path d="M31.4269 32H1.54146C0.692083 32 0 31.3158 0 30.4762V1.52381C0 0.68416 0.692083 0 1.54146 0H16.4842C17.3335 0 18.0256 0.68416 18.0256 1.52381V5.98639C18.0256 6.82604 17.3335 7.5102 16.4842 7.5102C15.6348 7.5102 14.9427 6.82604 14.9427 5.98639V3.07872H3.11437V28.9213H29.854V3.07872H26.0475C25.1981 3.07872 24.506 2.39456 24.506 1.55491C24.506 0.715257 25.1981 0.0310977 26.0475 0.0310977H31.4269C32.2762 0.0310977 32.9683 0.715257 32.9683 1.55491V30.4762C32.9683 31.3158 32.2762 32 31.4269 32Z" fill="#3A8FCD" class=""></path>
+                <path d="M13.548 10.6672C11.7706 10.6672 10.3235 12.0978 10.3235 13.8548C10.3235 17.4466 13.9884 18.3796 16.4736 21.9248C18.8644 18.4107 22.6394 17.3067 22.6394 13.8548C22.6394 12.0978 21.1923 10.6672 19.4149 10.6672C18.1252 10.6672 17.0241 11.4291 16.505 12.502C15.9703 11.398 14.8692 10.6672 13.548 10.6672Z" fill="#F65A5B" class=""></path>
+                <path d="M16.4895 22.0958C16.4423 22.0958 16.3637 22.0648 16.3322 22.0181C15.3727 20.6653 14.2402 19.7013 13.265 18.8305C11.5977 17.3845 10.1506 16.125 10.1506 13.8393C10.1506 11.9734 11.6921 10.4496 13.5796 10.4496C14.7593 10.4496 15.876 11.0871 16.4895 12.0822C17.1029 11.0871 18.2197 10.4496 19.3994 10.4496C21.2869 10.4496 22.8283 11.9734 22.8283 13.8393C22.8283 16.0317 21.3813 17.2756 19.7454 18.7217C18.723 19.608 17.5591 20.6342 16.6625 21.987C16.6153 22.0803 16.5367 22.0958 16.4895 22.0958ZM13.5481 10.8383C11.8808 10.8383 10.5124 12.1911 10.5124 13.8393C10.5124 15.954 11.8337 17.1046 13.4695 18.5196C14.429 19.3592 15.4986 20.2766 16.4266 21.5516C17.3231 20.2455 18.4399 19.2815 19.4151 18.4418C21.0195 17.0424 22.3722 15.8451 22.3722 13.8393C22.3722 12.1911 21.0038 10.8383 19.3365 10.8383C18.1725 10.8383 17.1029 11.5225 16.5839 12.5643C16.5524 12.642 16.4738 12.6731 16.4108 12.6731C16.3322 12.6731 16.2536 12.6265 16.2378 12.5643C15.8289 11.538 14.7435 10.8383 13.5481 10.8383Z" fill="#F65A5B"></path>
+            </svg>
+
+            <div class="ml-1">Wire transfer</div>
         </a>
     </div>
 
     <div class="mt-3">
         <table class="table table-bg text-left d-inline-block">
             <tbody>
-                <tr>
-                    <td class="text-nowrap">
-                        <i class="icon-ethereum"></i> 
-                        Chainlink
-                    </td>
-                    <td>
-                        <code class="text-break-all">0xc19be8972809b6b989f3e4ba16595712ba8e0de4</code>
-                    </td>
-                </tr>
                 <tr>
                     <td class="text-nowrap">
                         <i class="icon-ethereum"></i> 
